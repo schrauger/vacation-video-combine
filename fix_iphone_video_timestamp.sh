89e2cb49eef81bc8960909d7d5a52d057c15e482
Stephen Schrauger <stephen@schrauger.com>
2018-08-25 17:04:57 -0400

    iphone movie timestamp utility

---
+++ fix_iphone_video_timestamp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Sets the system modified timestamp of iphone MOV files, based
+# on the encoded date within the video metadata.
+# Requires 'mediainfo' to be installed.
+
+for filename in *.MOV ; do
+  creationdate=$(mediainfo "${filename}" | grep "Encoded date" | head -n 1 | cut -d ":" -f2- | xargs)
+  touch -d"${creationdate}"
+done
