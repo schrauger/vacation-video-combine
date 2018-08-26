f38ea506314f050d39142c7c2d154f6e5fa470d9
Stephen Schrauger <stephen@schrauger.com>
2018-08-25 23:28:12 -0400

    allow recursive searching

--- fix_iphone_video_timestamp.sh
+++ fix_iphone_video_timestamp.sh
@@ -3,7 +3,7 @@
 # on the encoded date within the video metadata.
 # Requires 'mediainfo' to be installed.
 
-for filename in *.MOV ; do
+find . -type f -iname "*.mov" -print0 | while IFS= read -r -d $'\0' filename; do
   creationdate=$(mediainfo "${filename}" | grep "Encoded date" | head -n 1 | cut -d ":" -f2- | xargs)
   echo "Setting system modified time for ${filename} to ${creationdate}"
   touch -d"${creationdate}" "${filename}"
